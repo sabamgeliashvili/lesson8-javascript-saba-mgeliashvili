bcbeb4aedf5bbf8f72711c86bad7356bcbd8e924
sabamgeliashvili <sba69717@gmail.com>
2022-09-12 13:30:48 +0400

    add task

--- script.js
+++ script.js
@@ -13,13 +13,31 @@ console.log(ispalindrome('madam'))
 //  Task 2
 
 
-let txt = "adsasdoasd"
-for( let i = 0 ; i < txt.length ; i++){
-    if(txt.indexOf(txt.charAt(i)) == txt.lastIndexOf(txt.charAt(i))){
-        console.log(txt.charAt(i))
-    }
-}
+// let txt = "adsasdoasd"
+// for( let i = 0 ; i < txt.length ; i++){
+//     if(txt.indexOf(txt.charAt(i)) == txt.lastIndexOf(txt.charAt(i))){
+//         console.log(txt.charAt(i))
+//     }
+// }
 
+
+//  // task 3
+
+const txt = "sad midis matarebeli"
+console.log(txt.split(" "));
+function firstCharupper(t){
+    return t[0].toUpperCase() + t.substring(1);
+}
+function camelize(x) {
+    let splittedArr = txt.split(" ");
+    let result = '';
+    splittedArr.forEach((val,index ) => {
+        if(index == 0 ) result = result + val;
+        else result = result + firstCharupper(val)
+    });
+    return result
+}
+console.log(camelize(txt))
 
 
 
@@ -39,5 +57,34 @@ function suffix(x){
 }
 let num1 = suffix(1)
 console.log(num1)
+
+
+  const users = [
+      {
+          name: "Angelina Jolie",
+          age: 80
+      },
+      {
+          name: "Eric Jones",
+          age: 2
+      },
+      {
+          name: "Paris Hilton",
+          age: 5
+      },
+      {
+          name: "Kayne West",
+          age: 16
+      },
+      {
+          name: "Bob Ziroll",
+          age: 100
+      }
+  ]; 
+ const x = users.map((users) => {
+let fullinfo = "<h1>" + users.name + "</h1>" + "<h2>" + users.age + "</h2>";
+return fullinfo
+ });
+ console.log(x)
 
 
